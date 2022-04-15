4c116972b7ea655872db84bd1313b17ad46f6418
gabriel velez <gabriel.dan.velez@gmail.com>
2022-04-15 19:08:05 -0400

    .

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,13 +1,22 @@
 import React from "react";
 
 import { render, screen } from "@testing-library/react";
-
+import "@testing-library/jest-dom";
 import AppFunctional from "../components/AppFunctional";
 // Write your tests here
 test("sanity", () => {
   expect(true).toBe(true);
+});
+
+test("Renders without errors", () => {
   render(<AppFunctional />);
-  const header = screen.queryByText(/welcome/i);
+});
+
+test("App render proper elements", () => {
+  // Arrange: render component
+  render(<AppFunctional />);
+
+  //Act:
   const coordinates = screen.queryByText(/coordinates/i);
   const steps = screen.queryByText(/you moved/i);
   const up = screen.queryByText(/up/i);
@@ -16,7 +25,7 @@ test("sanity", () => {
   const right = screen.queryByText(/right/i);
   const reset = screen.queryByText(/reset/i);
 
-  expect(header).toBeInTheDocument();
+  //Assert
   expect(coordinates).toBeInTheDocument();
   expect(steps).toBeInTheDocument();
   expect(up).toBeInTheDocument();
@@ -25,3 +34,22 @@ test("sanity", () => {
   expect(right).toBeInTheDocument();
   expect(reset).toBeInTheDocument();
 });
+
+// render(<AppFunctional />);
+// const header = screen.queryByText(/welcome/i);
+// const coordinates = screen.queryByText(/coordinates/i);
+// const steps = screen.queryByText(/you moved/i);
+// const up = screen.queryByText(/up/i);
+// const down = screen.queryByText(/down/i);
+// const left = screen.queryByText(/left/i);
+// const right = screen.queryByText(/right/i);
+// const reset = screen.queryByText(/reset/i);
+
+// expect(header).toBeInTheDocument();
+// expect(coordinates).toBeInTheDocument();
+// expect(steps).toBeInTheDocument();
+// expect(up).toBeInTheDocument();
+// expect(down).toBeInTheDocument();
+// expect(left).toBeInTheDocument();
+// expect(right).toBeInTheDocument();
+// expect(reset).toBeInTheDocument();
